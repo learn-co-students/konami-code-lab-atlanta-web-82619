fcef29e1a73f8bbb6ff07d704cda081421c3c191
Alexandria <alex.pugia816@hotmail.com>
2019-10-08 14:40:25 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,21 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0
+
+  document.body.addEventListener('keydown', (triggerKeyDown) => {
+    const key = triggerKeyDown.key
+
+    if (codes[index] === key) {
+      ++index
+      console.log(index)
+      if (index === 10) {
+        alert("ACHIEVEMENT GET - LEVEL UP")
+        index = 0
+      }
+    }
+    else {
+      index = 0
+    }
+  })
 }
